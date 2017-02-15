deeaffe70d2f889ca10acd5eecc2c67a00a2e4c0
Roberto Salgado <lightos@gmail.com>
2017-02-14 19:57:02 -0800

    possible bug fix

--- drumpfbot.py
+++ drumpfbot.py
@@ -393,6 +393,7 @@ class DrumpfBot:
                     print("  *Rotating player turn queue")
                     #rotate player_turn_queue until the first player is the one who won
                     self.player_turn_queue.rotate(1)
+                    self.player_bid_queue.rotate(1)
                 self.player_turn_queue_reference = copy.copy(self.player_turn_queue)
                 self.winner_for_sub_round = None
                 self.private_message_user(self.player_turn_queue[0], "Play a card `index`")
