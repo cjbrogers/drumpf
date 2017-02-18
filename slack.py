d3f02dd3555765455a169b9945289d18d3ae4f74
Charles JB Rogers II <cjbrogers@gmail.com>
2017-02-18 03:11:36 -0800

    still testing the App integration

--- slack.py
+++ slack.py
@@ -5,6 +5,8 @@ from flask_dance.contrib.slack import make_slack_blueprint, slack
 from flask_sslify import SSLify
 from raven.contrib.flask import Sentry
 
+from slackclient import SlackClient
+
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 sentry = Sentry(app)
