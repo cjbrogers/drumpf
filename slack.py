b79ff0c17c1f2bf9972fa1b9865cf79751f3f196
Charles JB Rogers II <cjbrogers@gmail.com>
2017-02-18 20:44:55 -0800

    testing complete?

--- slack.py
+++ slack.py
@@ -22,6 +22,12 @@ app.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 app.api_call = app.slack_client.api_call("users.list")
 app.slack_client.rtm_connect()
 
+@app.route("/responses/", methods=['POST'])
+def actions():
+    print request.text
+    payload={"text": "bananas"}
+    requests.post("https://hooks.slack.com/services/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",json=payload)
+
 @app.route("/actions/", methods=['POST'])
 def actions():
     print request.text
