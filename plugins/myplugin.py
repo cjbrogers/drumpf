e14f4f344026b823074b849d58a1a17aa04af04c
Charles JB Rogers II <cjbrogers@gmail.com>
2017-02-18 17:51:42 -0800

    testing complete?

--- plugins/myplugin.py
+++ plugins/myplugin.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from rtmbot.core import Plugin
 
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-
 class MyPlugin(Plugin):
 
     def catch_all(self, data):
