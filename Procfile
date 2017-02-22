3ad87fd7b722ddd2d67a04477dd98eeb357aa506
Charles JB Rogers II <cjbrogers@gmail.com>
2017-02-22 12:32:17 -0800

    consolidated oauth files in receive.py due to constraints on heroku dynos

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-web: python oauth.py
 web: python receive.py
 worker: python drumpfbot.py
--- receive.py
+++ receive.py
@@ -90,6 +90,52 @@ def inbound():
         slack.chat.post_message(channel=channel_id,text = AT_BOT +" {}".format(value),as_user=True)
     return Response(), 200
 
+# the beginning of the Sign In to Slack OAuth process.
+# we can get the user tokens from the return of this call
+@app.route("/signin", methods=["GET"])
+def pre_signin():
+    return '''
+      <a href="https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=122416745729.127817802451">
+
+          <img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" />
+
+      </a>
+    '''
+    
+@app.route("/signin/finish", methods=["GET", "POST"])
+def post_signin():
+    # Retrieve the auth code from the request params
+    auth_code = request.args['code']
+
+    # An empty string is a valid token for this request
+    sc = SlackClient("")
+
+    # Request the auth tokens from Slack
+    auth_response = sc.api_call(
+        "oauth.access",
+        client_id=client_id,
+        client_secret=client_secret,
+        code=auth_code
+    )
+
+    # Save the bot token to an environmental variable or to your data store
+    # for later use
+    print(auth_response)
+    token = auth_response['access_token']
+    uid = auth_response['user']['id']
+    name = auth_response['user']['name']
+
+    values = {"token": token, "uid": uid, "name": name}
+    df = pd.DataFrame(values, index=[0])
+    engine = create_engine('mysql+pymysql://root:jamesonrogers@localhost:3306/drumpf', echo=False)
+    df.to_sql(con=engine, name='user', if_exists='append', index=False)
+
+    # TODO: add the above auth_response elements to a DB
+    # send_to_db(token,uid,name)
+
+    # Don't forget to let the user know that auth has succeeded!
+    return "<h1>Welcome to Drumpf!</h1> You can now close this window and focus on making card games great again."
+
 @app.route('/', methods=['GET'])
 def test():
     return Response('It works!')
