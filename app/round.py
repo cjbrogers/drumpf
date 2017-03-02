024fd2d57d592ba0e294273c6ec67506e8390f17
Charles JB Rogers II <cjbrogers@gmail.com>
2017-03-01 17:59:56 -0800

    making card games great again

--- app/round.py
+++ app/round.py
@@ -173,7 +173,8 @@ class Round():
             print " ",msg
             self.score.build_scoreboard(msg)
             self.score.update_scoreboard(self.bot.scoreboard)
-            self.score.pm_users_scoreboard(self.bot.scoreboard)
+            for player_id in self.bot.users_in_game:
+                self.bot.private_message_user(player_id,msg)
 
             #reset all sub-round variables
             self.bot.leading_suit = None
@@ -192,7 +193,9 @@ class Round():
                 msg = ">_Sub-Round {}_\n".format(self.bot.sub_rounds_played + 1)
                 self.score.build_scoreboard(msg)
                 self.score.update_scoreboard(self.bot.scoreboard)
-                self.score.pm_users_scoreboard(self.bot.scoreboard)
+                # self.score.pm_users_scoreboard(self.bot.scoreboard)
+                for player_id in self.bot.users_in_game:
+                    self.bot.private_message_user(player_id,msg)
 
                 #initialize another turn queue cause there are more cards to play
                 self.bot.player_turn_queue = copy.copy(self.bot.users_in_game)
