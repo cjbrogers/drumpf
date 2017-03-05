98dfb502c460ead18975108fbdc57109a77ac68b
Charles JB Rogers II <cjbrogers@gmail.com>
2017-03-05 15:14:23 -0800

    retrieving OAuth permissions from db instead of ENV

--- app/models.py
+++ app/models.py
@@ -70,8 +70,8 @@ def get_access_token(user_id):
             data = (user_id)
             cursor.execute(sql,data)
             token = cursor.fetchall()
-            print token
-            return token
+            print token['access_token']
+            return token['access_token']
             # for user in users:
             #     print user
                 # if user['user_id'] == user_id:
