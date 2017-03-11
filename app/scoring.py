f90474fbdf1fab2cbbb10cf973fefa53f03ee7df
Charles JB Rogers II <cjbrogers@gmail.com>
2017-03-10 23:57:55 -0800

    hardcoding channel for pm_scoreboard testing

--- app/scoring.py
+++ app/scoring.py
@@ -126,7 +126,7 @@ class Scoring():
             ts = models.get_ts(player_id,"pm_scoreboard",self.bot.team_id)
             resp = self.slack_client.api_call(
                 "chat.update",
-                channel=player_id,
+                channel="D4F6RHNE8",
                 text=board,
                 ts=ts,
                 as_user=True,
